c269258845a708bf8110e6ece12b1de70c00b36a
RithvikR1218 <rithvik.ravilla@gmail.com>
2024-05-01 12:00:20 +0530

    update: handshake for 2 way communication

--- AES/server.py
+++ AES/server.py
@@ -1,7 +1,7 @@
 import socket
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import unpad
+from Crypto.Util.Padding import unpad,pad
 
 def run_server():
     # create a socket object
@@ -17,20 +17,35 @@ def run_server():
     
     print(f"Listening on {server_ip}:{port}")
 
-    #Set up the Keys
-    key = get_random_bytes(16)    
+
     # accept connection from client    
     client_socket, client_address = server.accept()
 
     print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
-    
+
+    #AES Handshake
+
+    #Set up the Keys
+    key = get_random_bytes(16)    
     client_socket.send(key)
+
+    #Receive IV1
     iv = client_socket.recv(1024)
     cipher = AES.new(key, AES.MODE_CBC,iv)
 
+    #Send IV2
+    cipher2 = AES.new(key,AES.MODE_CBC)
+    client_socket.send(cipher2.iv)
+
     while True:
+        #Request Handling
         ciphertext = client_socket.recv(1024)
         pt = unpad(cipher.decrypt(ciphertext), AES.block_size)
         print("The message was: ", pt.decode('utf-8'))
+        response = "Received".encode('utf-8')
+
+        #Sending Response
+        ct_bytes = cipher2.encrypt(pad(response, AES.block_size))
+        client_socket.send(ct_bytes)
 
 run_server()
