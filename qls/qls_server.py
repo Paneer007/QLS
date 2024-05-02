8a521975299a33116b6eaa6e5a7399ff3031c7e0
Sanjai <sanjaikumar.mani@gmail.com>
2024-05-02 10:38:22 +0530

    feat: added  added docs + comments

--- qls/qls_server.py
+++ qls/qls_server.py
@@ -1,20 +1,37 @@
-from qiskit import QuantumCircuit, execute, Aer
+from qiskit import QuantumCircuit
 from numpy.random import randint
 import numpy as np
 import socket
 import pickle
 from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import unpad, pad
+from Crypto.Util.Padding import unpad
 
 NUM_QUBITS = 96
 
 
 def generate_bits(n: int) -> np.ndarray:
+    """
+    Generates an array of random binary bits.
+
+    Parameters:
+        n (int): The length of the array, indicating the number of bits to generate.
+
+    Returns:
+        np.ndarray: An array of random binary bits of length 'n'.
+    """
     return randint(2, size=n)
 
 
-def four_fold_key(key):
+def four_fold_key(key: list) -> list:
+    """
+    Splits a key into four equal parts and performs XOR operation on each part.
+
+    Parameters:
+        key (list): The input key to be processed.
+
+    Returns:
+        list: A list containing four values obtained by XOR-ing every fourth element of the input key.
+    """
     leng = len(key)
     dist = leng//4
     arr = []
@@ -30,6 +47,16 @@ def four_fold_key(key):
 
 
 def encode_message(bits: np.ndarray, basis: np.ndarray) -> list:
+    """
+    Encodes a message using quantum circuits based on given bits and bases.
+
+    Parameters:
+        bits (np.ndarray): An array containing the bits to be encoded.
+        basis (np.ndarray): An array indicating the bases for encoding.
+
+    Returns:
+        list: A list of quantum bits representing the encoded message.
+    """
     message = []
     for i in range(NUM_QUBITS):
         # 1 qubit and 1 classical bit - message and measurement respectively
@@ -39,30 +66,60 @@ def encode_message(bits: np.ndarray, basis: np.ndarray) -> list:
                 qc.x(0)  # Bit flip (Pauli-X gate)
         else:  # Prepare qubit in X-basis (i.e. |+> or |->)
             if bits[i] == 0:
-                qc.h(0)  # Hadamard gate
+                qc.h(0)  # Hadamard gate for |+>
             else:
                 qc.x(0)
-                qc.h(0)
+                qc.h(0)  # Hadamard + Pauli-X gate for |->
         qc.barrier()
         message.append(qc)
     return message
 
 
 def simulate_quantum_channel(message: list, error_rate: float) -> list:
+    """
+    Simulates a quantum channel by introducing errors to the encoded message.
+
+    Parameters:
+        message (list): A list of quantum circuits representing the encoded message.
+        error_rate (float): The probability of introducing an error to each qubit.
+
+    Returns:
+        list: A list of quantum bits representing the noisy message after error introduction.
+    """
     noisy_message = []
     for qc in message:
-        if np.random.random() < error_rate:
+        if np.random.random() < error_rate:  # randomly creates a probability and checks if it is less than the threshold
             qc.x(0)
         noisy_message.append(qc)
     return noisy_message
 
 
 def remove_garbage(a_basis: np.ndarray, b_basis: np.ndarray, bits: np.ndarray) -> list:
+    """
+    Removes garbage bits from the received bits based on matching bases.
+
+    Parameters:
+        a_basis (np.ndarray): An array representing Alice's measurement bases.
+        b_basis (np.ndarray): An array representing Bob's measurement bases.
+        bits (np.ndarray): An array containing the received bits.
+
+    Returns:
+        list: A list containing the bits that were measured with matching bases by Alice and Bob.
+    """
     return [bits[q] for q in range(NUM_QUBITS) if a_basis[q] == b_basis[q]]
 
 
-def check_keys(map, key2: list) -> bool:
-    global flag
+def check_keys(map: dict, key2: list) -> bool:
+    """
+    Checks if two keys are identical.
+
+    Parameters:
+        map (dict): A dictionary representing a key mapping of announced public key.
+        key2 (list): The Alice key to compare with the key in the mapping.
+
+    Returns:
+        bool: True if the keys are identical, False otherwise.
+    """
     flag = True
 
     for key, value in map.items():
@@ -77,7 +134,16 @@ def check_keys(map, key2: list) -> bool:
     return flag
 
 
-def recv_stream(conn):
+def recv_stream(conn: any) -> any:
+    """
+    Receive streamed data over a connection.
+
+    Parameters:
+        conn: The connection object.
+
+    Returns:
+        The received data.
+    """
     received_data = b""
     while True:
         str = conn.recv(1024)
@@ -90,83 +156,168 @@ def recv_stream(conn):
     return data
 
 
-def send_stream(conn, data_chunk):
-    ssm_dump = pickle.dumps(data_chunk)
+def send_stream(conn, data_chunk) -> None:
+    """
+    Send data chunk over a connection in a streamed manner.
+
+    Parameters:
+        conn: The connection object.
+        data_chunk: The data chunk to be sent.
+    """
+    data_dump = pickle.dumps(
+        data_chunk)  # enccoding the data to a binary format
     bytes_sent = 0
-    while bytes_sent < len(ssm_dump):
-        chunk = ssm_dump[bytes_sent:bytes_sent+4096]
+    # keep sending data till all the data is transmitted
+    while bytes_sent < len(data_dump):
+        chunk = data_dump[bytes_sent:bytes_sent+4096]
         conn.sendall(chunk)
         bytes_sent += len(chunk)
     conn.send(b"done")
 
 
 class QLS_Server:
-    # Default Line Ending
-    lineending = "\n"
 
     def __init__(self) -> None:
+        """
+            Initializes a QLS Server instance.
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def bind(self, host, port):
+    def bind(self, host, port) -> None:
+        """
+        Binds the server to a specified host and port.
+
+        Parameters:
+            host (str): The host address to bind.
+            port (int): The port number to bind.
+        """
         self._host = host
         self._port = port
         self.socket.bind((host, port))
         self.socket.listen(0)
 
-    def aes_connect(self, conn, addr):
-        iv1_key = recv_stream(conn)
+    def aes_connect(self, conn) -> None:
+        """
+        Establishes an AES connection.
+
+        Parameters:
+            conn: The connection object.
+            addr: The address of the connected client.
+        """
+        iv1_key = recv_stream(conn)  # obtain initialisation vector from client
+        # creates decryption cipher
         self.cipher1 = AES.new(bytes(self.aes_key), AES.MODE_CBC, iv1_key)
+        # creates encryption cipher
         self.cipher2 = AES.new(bytes(self.aes_key), AES.MODE_CBC)
         send_stream(conn, self.cipher2.iv)
-        pass
 
-    def recv_data(self, conn, addr):
+    def recv_data(self, conn) -> None:
+        """
+        Receives encrypted data and decrypts it.
+
+        Parameters:
+            conn: The connection object.
+            addr: The address of the connected client.
+        """
         ciphertext = recv_stream(conn)
         pt = unpad(self.cipher1.decrypt(ciphertext), AES.block_size)
         print("The message was: ", pt.decode('utf-8'))
 
-    def qls_connect(self, conn, addr):
+    def qls_connect(self, conn) -> None:
+        """
+        Establishes a QLS connection.
+
+        Parameters:
+            conn: The connection object.
+            addr: The address of the connected client.
+        """
+        # Generate secret to be sent via quantum channel
         secret = generate_bits(NUM_QUBITS)
-        # TODO: implement four tone key checking
-        secret_basis = generate_bits(NUM_QUBITS)
-        secret_message = encode_message(secret, secret_basis)
-        secret_sent_message = simulate_quantum_channel(secret_message, 0.01)
+
+        # Generate Alice's basis vector
+        alice_basis = generate_bits(NUM_QUBITS)
+
+        # Encode the secret using quantum states
+        secret_quantum_encoded = encode_message(secret, alice_basis)
+
+        # Simulate noise in the quantum channel
+        secret_sent_message = simulate_quantum_channel(
+            secret_quantum_encoded, 0.01)
+
         with conn:
+            # Send the secret message via the quantum channel
             send_stream(conn, secret_sent_message)
+
+            # Receive Bob's basis
             bob_basis = recv_stream(conn)
-            send_stream(conn, secret_basis)
-            alex_key = remove_garbage(secret_basis, bob_basis, secret)
+
+            # Send Alice's basis to Bob
+            send_stream(conn, alice_basis)
+
+            # Check if any of the keys do not match
+            alice_key = remove_garbage(alice_basis, bob_basis, secret)
+
+            # Obtain publicly announced key from Bob
             bob_map_key = recv_stream(conn)
-            res = check_keys(bob_map_key, alex_key)
+
+            # Check if the keys match
+            res = check_keys(bob_map_key, alice_key)
             status = ""
             if res:
-                alex_key = four_fold_key(alex_key)
-                self.aes_key = alex_key
+                # Perform 4-fold key compression
+                alice_key = four_fold_key(alice_key)
+                self.aes_key = alice_key
                 status = "validdone"
             else:
                 status = "repeatdone"
             send_stream(conn, status)
 
-    # establishes QLS with the client before sending more requests
-
     def listen_and_accept(self):
+        """
+        Listens for incoming connections and accepts them.
+        """
         while True:
             conn, addr = self.socket.accept()
+            print(f"connected by {addr}")
             type = conn.recv(1024).decode()
+            # when client wants to connect to the server
             if type == "connectdata":
-                self.qls_connect(conn, addr)
+                self.qls_connect(conn)
+            # when client wants to perform aes des exchange
             elif type == "aesconnectdata":
-                self.aes_connect(conn, addr)
+                self.aes_connect(conn)
+            # when client wants to send messages back and forth to the server
             elif type == "senddata":
-                self.recv_data(conn, addr)
+                self.recv_data(conn)
             else:
                 print("Skill issue")
 
     def send(self, message: str) -> None:
+        """
+        Sends a message.
+
+        Parameters:
+            message (str): The message to send.
+        """
         self.socket.send(message.encode())
 
     def send_bytes(self, message: bytes) -> None:
+        """
+        Sends bytes.
+
+        Parameters:
+            message (bytes): The bytes to send.
+        """
         self.socket.send(message)
 
     def recv(self, bufsize=1024) -> str:
+        """
+        Receives a message.
+
+        Parameters:
+            bufsize (int): The buffer size for receiving.
+
+        Returns:
+            str: The received message.
+        """
         return self.socket.recv(bufsize).decode()
--- qls/qs_client.py
+++ qls/qs_client.py
@@ -1,4 +1,4 @@
-from qiskit import QuantumCircuit, execute, Aer
+from qiskit import execute, Aer
 from numpy.random import randint
 import numpy as np
 import socket
@@ -6,12 +6,21 @@ import pickle
 import random
 
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.Padding import pad
 
 NUM_QUBITS = 96
 
 
-def select_random_indices(arr):
+def select_random_indices(arr: list) -> list:
+    """
+    Randomly selects indices from an array.
+
+    Parameters:
+        arr (list): The array from which indices will be selected.
+
+    Returns:
+        dict: A dictionary containing randomly selected indices and their corresponding values.
+    """
     num_indices = len(arr) // 4
     random_indices = random.sample(range(len(arr)), num_indices)
     result = {index: arr[index] for index in random_indices}
@@ -19,27 +28,70 @@ def select_random_indices(arr):
 
 
 def generate_bits(n: int) -> np.ndarray:
+    """
+    Generates an array of random binary bits.
+
+    Parameters:
+        n (int): The length of the array, indicating the number of bits to generate.
+
+    Returns:
+        np.ndarray: An array of random binary bits of length 'n'.
+    """
     return randint(2, size=n)
 
 
 def measure_message(message: list, basis: np.ndarray) -> list:
-    backend = Aer.get_backend("qasm_simulator")
+    """
+    Measures a quantum message.
+
+    Parameters:
+        message (list): A list of quantum bits representing the message.
+        basis (np.ndarray): An array indicating the measurement bases.
+
+    Returns:
+        list: A list containing the measured results.
+    """
+    backend = Aer.get_backend(
+        "qasm_simulator")  # obtain the simulator for noise in the qubits
     measurements = []
     for q in range(NUM_QUBITS):
-        if basis[q] == 1:  # Measuring in X-basis
+        if basis[q] == 1:  # Check if the measurement basis is 1 (X-basis)
+            # Apply a Hadamard gate to prepare the qubit in the X-basis
             message[q].h(0)
+        # Measure the qubit and store the result in classical bit 0
         message[q].measure(0, 0)
+        # Execute the quantum circuit
         result = execute(message[q], backend, shots=1, memory=True).result()
+        # Append the measurement result to the list of measurements
         measurements.append(int(result.get_memory()[0]))
     return measurements
 
 
 def remove_garbage(a_basis: np.ndarray, b_basis: np.ndarray, bits: np.ndarray) -> list:
-    # Removes bits that do not match
+    """
+    Removes garbage bits based on matching bases.
+
+    Parameters:
+        a_basis (np.ndarray): Alice's measurement bases.
+        b_basis (np.ndarray): Bob's measurement bases.
+        bits (np.ndarray): The received bits.
+
+    Returns:
+        list: The filtered bits.
+    """
     return [bits[q] for q in range(NUM_QUBITS) if a_basis[q] == b_basis[q]]
 
 
-def recv_stream(conn):
+def recv_stream(conn: any) -> any:
+    """
+    Receives streamed data over a connection.
+
+    Parameters:
+        conn: The connection object.
+
+    Returns:
+        Any: The received data.
+    """
     received_data = b""
     while True:
         str = conn.recv()
@@ -53,7 +105,14 @@ def recv_stream(conn):
     return data
 
 
-def send_stream(conn, data_chunk):
+def send_stream(conn, data_chunk: any) -> None:
+    """
+    Sends data chunk over a connection in a streamed manner.
+
+    Parameters:
+        conn (socket.socket): The connection object.
+        data_chunk (Any): The data chunk to be sent.
+    """
     ssm_dump = pickle.dumps(data_chunk)
     bytes_sent = 0
     while bytes_sent < len(ssm_dump):
@@ -63,7 +122,16 @@ def send_stream(conn, data_chunk):
     conn.send(b"done")
 
 
-def four_fold_key(key):
+def four_fold_key(key: list) -> list:
+    """
+    Performs four-fold key transformation. Allows us to mask bits which are publically announced
+
+    Parameters:
+        key (list): The key to be transformed.
+
+    Returns:
+        list: The transformed key.
+    """
     leng = len(key)
     dist = leng//4
     arr = []
@@ -79,56 +147,106 @@ def four_fold_key(key):
 
 
 class QLS_Client:
-    # Default Line Ending
-    lineending = "\n"
 
     def __init__(self) -> None:
+        """
+        Initializes a QLS Client instance.
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def send_data(self, host, port):
+        """
+        Sends data over a connection.
+
+        Parameters:
+            host (str): The host address.
+            port (int): The port number.
+        """
         self.socket.connect((host, port))
         self.socket.send(b"senddata")
-        
+
         data = input("> ")
+
+        # Encrypt the input data using AES encryption with the cipher1 object
         ct_bytes = self.cipher1.encrypt(
             pad(data.encode('utf-8'), AES.block_size))
-        print("CT Text: ",ct_bytes)
+
+        print("CT Text: ", ct_bytes)
         send_stream(self.socket, ct_bytes)
         self.socket.close()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        #response = recv_stream(self)
-        #print(response)
 
     def aes_connect(self, host, port):
+        """
+        Establishes an AES connection.
+
+        Parameters:
+            host (str): The host address.
+            port (int): The port number.
+        """
         self.socket.connect((host, port))
         self.socket.send(b"aesconnectdata")
+
+        # Initialize cipher1 object with AES encryption using the secret key in CBC mode
         self.cipher1 = AES.new(bytes(self.secret_key), AES.MODE_CBC)
+
+        # Send the initialization vector (IV) of cipher1 over the socket using send_stream function
         send_stream(self.socket, self.cipher1.iv)
+
+        # Receive the initialization vector (IV) for cipher2 from the socket using recv_stream function
         iv2 = recv_stream(self)
+
+        # Initialize cipher2 object with AES encryption using the secret key and received IV in CBC mode
         self.cipher2 = AES.new(bytes(self.secret_key), AES.MODE_CBC, iv2)
+
         self.socket.close()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def qkd_connect(self, host, port):
+        """
+        Establishes a QKD connection.
+
+        Parameters:
+            host (str): The host address.
+            port (int): The port number.
+        """
         self._host = host
         self._port = port
 
+        # Iterate 5 times to attempt connection
         for i in range(0, 5):
             self.socket.connect((host, port))
             self.socket.send(b"connectdata")
-            received_list = recv_stream(self)
+
+            # Receive alice from the socket and store it as a list
+            alice_message = recv_stream(self)
+
+            # Generate random bit string as Bob's measurement basis
             bob_basis = generate_bits(NUM_QUBITS)
-            bob_results = measure_message(received_list, bob_basis)
+
+            # Measure the received quantum message using Bob's basis
+            bob_results = measure_message(alice_message, bob_basis)
+
+            # Send Bob's measurement basis over the socket
             send_stream(self.socket, bob_basis)
-            alex_basis = recv_stream(self)
-            bob_key = remove_garbage(alex_basis, bob_basis, bob_results)
+
+            # Receive Alice's measurement basis from the socket
+            alice_basis = recv_stream(self)
+
+            # Remove garbage bits and obtain Bob's key
+            bob_key = remove_garbage(alice_basis, bob_basis, bob_results)
+
+            # Randomly select bits from Bob's key to create a publicly announced map key
             bob_map_key = select_random_indices(bob_key)
+
+            # Send Bob's map key over the socket
             send_stream(self.socket, bob_map_key)
+
             response = recv_stream(self)
             self.socket.close()
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             if response == "validdone":
+                # Perform 4-fold key compression
                 bob_key = four_fold_key(bob_key)
                 self.secret_key = bob_key
                 break
@@ -140,7 +258,22 @@ class QLS_Client:
             print("Connection succeeded")
 
     def send(self, message: str) -> None:
+        """
+        Sends a message.
+
+        Parameters:
+            message (str): The message to send.
+        """
         self.socket.send(message.encode())
 
     def recv(self, bufsize=1024) -> str:
+        """
+        Receives a message.
+
+        Parameters:
+            bufsize (int): The buffer size for receiving.
+
+        Returns:
+            str: The received message.
+        """
         return self.socket.recv(bufsize)
